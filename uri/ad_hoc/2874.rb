253f18a66a7c21b0ccfcd1115a14a92be68bcdb2
Diogo Loiola <diogomouraloiola@gmail.com>
2023-02-13 09:21:48 -0300

    add 2874 uri

--- uri/ad_hoc/2874.rb
+++ uri/ad_hoc/2874.rb
@@ -1,7 +1,9 @@
 while line = gets
-  string = ''
-  (0..line.to_i - 1).each do
-    string += gets.to_i(2).chr
-  end
-  puts string
+  size = line.chomp.to_i
+
+  new_string = (1..size).map do
+    gets.chomp.to_i(2).chr
+  end.join
+
+  puts new_string
 end
